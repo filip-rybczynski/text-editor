48411b50cdb2d0fb8fa5288e85bafb7e01d5bdcb
Filip Rybczyński <filipryba@gmail.com>
2020-09-09 00:07:06 +0200

    feat: basic save and load functionality

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,31 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log('HELLO 🚀');
+
+const entry = localStorage.getItem('entry');
+let result = '';
+
+if (entry) {
+    result = entry;
+}
+
+
+let editor = document.querySelector(".editor__textarea");
+
+const loadButton = document.querySelector(".load--js");
+const saveButton = document.querySelector(".save--js");
+
+loadButton.addEventListener("click", ()=>{
+    const loadedText = localStorage.getItem('savedText');
+    // let content = editor.value;
+    // console.log(content);
+    // console.log("Loading");
+    editor.value = loadedText;
+});
+
+saveButton.addEventListener("click", ()=>{
+// let content = editor.value;
+localStorage.setItem('savedText', editor.value);
+// console.log("Saved successfully");
+});
