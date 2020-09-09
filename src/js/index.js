95890ff92d3158cc946c67984609a6499431f4f1
Filip Rybczyński <filipryba@gmail.com>
2020-09-09 23:41:10 +0200

    feat: sidebar button functionality

--- src/js/index.js
+++ src/js/index.js
@@ -20,6 +20,12 @@ let editor = document.querySelector(".editor__textarea");
 
 const loadButton = document.querySelector(".load--js");
 const saveButton = document.querySelector(".save--js");
+const boldButton = document.querySelector(".bold--js");
+const cursiveButton = document.querySelector(".cursive--js");
+const blueButton = document.querySelector(".blue--js");
+const greenButton = document.querySelector(".green--js");
+const redButton = document.querySelector(".red--js");
+const defaultButton = document.querySelector(".default--js");
 
 loadButton.addEventListener("click", ()=>{
     const loadedText = localStorage.getItem('savedText');
@@ -34,3 +40,37 @@ saveButton.addEventListener("click", ()=>{
 localStorage.setItem('savedText', editor.value);
 // console.log("Saved successfully");
 });
+
+boldButton.addEventListener("click", ()=>{
+    editor.classList.toggle("editor__textarea--bold");
+})
+
+cursiveButton.addEventListener("click", ()=>{
+    editor.classList.toggle("editor__textarea--cursive");
+})
+
+blueButton.addEventListener("click", ()=>{
+    editor.classList.toggle("editor__textarea--blue");
+    editor.classList.remove("editor__textarea--green");
+    editor.classList.remove("editor__textarea--red");
+})
+
+greenButton.addEventListener("click", ()=>{
+    editor.classList.toggle("editor__textarea--green");
+    editor.classList.remove("editor__textarea--blue");
+    editor.classList.remove("editor__textarea--red");
+})
+
+redButton.addEventListener("click", ()=>{
+    editor.classList.toggle("editor__textarea--red");
+    editor.classList.remove("editor__textarea--green");
+    editor.classList.remove("editor__textarea--blue");
+})
+
+defaultButton.addEventListener("click", ()=>{
+    editor.classList.remove("editor__textarea--blue");
+    editor.classList.remove("editor__textarea--green");
+    editor.classList.remove("editor__textarea--red");
+    editor.classList.remove("editor__textarea--cursive");
+    editor.classList.remove("editor__textarea--bold");
+})
