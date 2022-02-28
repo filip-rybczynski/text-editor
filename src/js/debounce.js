77a27fc5a693e2b48672940c45974af9ae7fe81d
Filip Rybczyński <filipryba@gmail.com>
2022-03-01 00:28:38 +0100

    Added typing animation

--- src/js/debounce.js
+++ src/js/debounce.js
@@ -1,7 +1,30 @@
+// const dotContainer = document.createElement("span");
+// dotContainer.className = 'dot';
+// dotContainer.textContent = "."
+
+const dots = new Array(3).fill(document.createElement("span"));
+console.log(dots);
+
+const editMessage = document.createElement("span");
+editMessage.textContent = "Editing";
+
+for (let i = 0; i < 3; i++) {
+  const dot = document.createElement("span");
+  dot.className = 'dot';
+  dot.textContent = ".";
+  editMessage.append(dot);
+}
+
+
+
 export default function debounce(fn, delay, saveTracker) {
     let timer;
     const autoSaving = () => {
-        saveTracker.innerHTML = "Editing...";
+      if(saveTracker.innerHTML === '' || saveTracker.innerHTML === 'Saved!!' ) {
+        saveTracker.innerHTML = '';
+        saveTracker.appendChild(editMessage);
+      }
+        
       clearTimeout(timer);
       timer = setTimeout(() => {
         fn()
